4c6ea172dab74198dcfe5740b2c2ba2aee6b8d4b
Hristo Dimitrov Yordanov <mrhristo29@gmail.com>
2024-11-11 21:00:58 +0200

    Create 'usermanagement' app.

---
+++ src/test/java/com/hristo/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hristo.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
